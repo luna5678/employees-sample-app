d09f02fbccb85363b803a8dce6b010d37898ddd1
lonnalu <lonnalu@gmail.com>
2022-01-21 14:22:27 -0800

    Attempts testing

--- src/components/employees/Employees.test.js
+++ src/components/employees/Employees.test.js
@@ -1,14 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Employees from './Employees'
 
-test('renders employee from json response', async () => {
-  window.fetch = jest.fn();
-  window.fetch.mockResolvedValueOnce({
-    json: async () => [{id: '123abc', firstName: 'Sam'}, {id: '123abd', firstName: 'Manny'}],
-  });
+// Tests do not pass at the moment
 
-  render(<Employees />);
-  
-  const employees = await screen.findAllByRole('heading', { level: 3 }, { timeout: 3000 });
-  expect(employees).not.toHaveLength(0);
+describe('Employees component', () => {
+  test('renders Current Employees heading', () => {
+    render(<Employees />);
+    const currentEmployeesElement = screen.getByText('Current Employees', { exact: true })
+    expect(currentEmployeesElement).toBeInTheDocument();
+  })
+
+  test('renders employee from json response', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{id: '123abc', firstName: 'Sam'}, {id: '123abd', firstName: 'Manny'}],
+    });
+
+    render(<Employees />);
+    const employees = await screen.findAllByRole('heading', { level: 3 }, { timeout: 3000 });
+    expect(employees).not.toHaveLength(0);
+  })
 })
